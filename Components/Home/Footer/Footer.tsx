2b980fe33664baee8be306541f8829a9c571308d
dani <dani@danis-MacBook-Pro.local>
2022-07-01 23:47:31 +0300

    added the home header and footer components

---
+++ Components/Home/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import './Footer.module.css'
+
+const Footer = () => {
+  return <footer>i am a footer</footer>
+}
+
+export default Footer
---
+++ Components/Home/Header/Header.tsx
@@ -0,0 +1,10 @@
+import './Header.module.css'
+// import Logo from './../../../Assets/Logo.svg'
+
+const Header = () => {
+  return <header id="header">
+            <h1>NETFLIX</h1>
+        </header>
+}
+
+export default Header

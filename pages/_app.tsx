434a26693b12902a0946e107a6dee5af61808472
dani <dani@danis-MacBook-Pro.local>
2022-07-01 23:00:43 +0300

    configurate the NProgress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+// activate the NProgress fot the page.
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
+NProgress.configure({ showSpinner: true })
 
 function MyApp ({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />

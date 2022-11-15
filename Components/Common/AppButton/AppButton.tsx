4aa27a8702c25363f77acac65d1f85b7fc08f633
daniel salahi <dan.salahii@gmail.com>
2022-11-15 22:49:29 +0300

    refactored the app button

--- Components/Common/AppButton/AppButton.tsx
+++ Components/Common/AppButton/AppButton.tsx
@@ -1,23 +1,35 @@
-import React, { ButtonHTMLAttributes } from 'react'
-import './AppButton.styles.scss'
+import React, { FC, ButtonHTMLAttributes } from 'react'
+import AppButtonSpinner from './AppButtonSpinner'
 
-type AppButtonProps = {
+export type AppButtonProps = {
   type: 'submit' | 'reset' | 'button'
   variant: 'primary' | 'secondary' | 'tertiary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark'
   children: React.ReactElement | string
   extendClass?: string
   isLoading?: boolean;
+  isFull?: boolean;
   onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-const AppButton: React.FC<AppButtonProps> = ({ onClick, variant, type, children, extendClass, isLoading, ...otherProps }) => {
-  return <button
+const baseBtnClass = 'text-center p-2 text-white transition-colors rounded-md cursor-pointer focus:outline-none focus:ring-blue-300 text-sm' +
+  ' focus:ring-2 '
+
+const PrimaryClasses = ' bg-primary hover:bg-blue-700'
+const DangerClasses = ' bg-red-500 hover:bg-red-600'
+
+const AppButton: FC<AppButtonProps> = ({ onClick, variant, type, children, extendClass, isLoading, isFull, ...otherProps }) => {
+  let classes = baseBtnClass + `${variant === 'primary' ? PrimaryClasses : (variant === 'danger' ? DangerClasses : '')} ${extendClass} ${isLoading ? 'cursor-not-allowed opacity-75 p-0' : ''}`
+  classes += isFull ? ' w-full' : ''
+  return (
+    <button
       onClick={onClick}
       type={type}
       disabled={isLoading}
+      className={classes}
       {...otherProps}>
-    {children}
-  </button>
+      {isLoading ? <AppButtonSpinner /> : children}
+    </button>
+  )
 }
 
 export default AppButton
